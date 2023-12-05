9400ddaf63a8a2c310700878eb0799bed133a098
David <Rigby335@hotmail.com>
2023-12-05 15:26:47 -0800

    feat: #GET all test fails RED (truncate clears db as expected)

--- tests/quotes.test.ts
+++ tests/quotes.test.ts
@@ -10,3 +10,13 @@ it('#POST connects to server', async () => {
   const res = await request(app).post('/quotes');
   expect(res.status).toBe(200);
 });
+it.only('#GET returns all rows from quotes table', async () => {
+  const res = await request(app).get('/quotes');
+  expect(res.status).toBe(200);
+  expect(res.body).toEqual({
+    id: expect.any(Number),
+    film: expect.any(String),
+    author: expect.any(String),
+    quote: expect.any(String),
+  });
+});
