f1ddb1e22d6d40f256ed2a07ceea8b6a4adc5aee
David <Rigby335@hotmail.com>
2023-12-06 12:10:40 -0800

    feat: seeded db with additional quotes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 import { Quote } from '../src/types';
 import { prisma } from '../src/utils/db.server';
 
@@ -16,11 +17,139 @@ async function seed() {
 }
 
 seed();
-
+// {film: 'Die Hard', author: '', quote: ''}
 function getQuotes(): Array<Quote> {
   return [
-    { film: 'Die Hard', author: 'John McClane', quote: 'Hello there' },
-    { film: 'Die Hard', author: 'Hans Gruber', quote: 'Good bye' },
+    {
+      film: 'Die Hard',
+      author: 'John McClane',
+      quote: "Come out to the coast, we'll get together, have a few laughs..",
+    },
+    {
+      film: 'Die Hard',
+      author: 'Hans Gruber',
+      quote: 'Now I have a machine gun. Ho ho ho.',
+    },
+    {
+      film: 'Die Hard',
+      author: 'John and Hans',
+      quote:
+        'Hans: This time John Wayne does not walk off into the sunset with Grace Kelly. John: That was Gary Cooper, jerk',
+    },
+    {
+      film: 'Die Hard',
+      author: 'John McClane',
+      quote: 'Welcome to the party, pal!',
+    },
+    {
+      film: 'Die Hard',
+      author: 'John and Hans',
+      quote:
+        "John: You'd have made a pretty good cowboy yourself, Hans. Hans: Oh, yes. What was it you said to me before? Yippee-ki-yay.",
+    },
+    {
+      film: 'Die Hard',
+      author: 'Dwayne T. Robinson',
+      quote: "We're gonna need some more FBI guys, I guess.",
+    },
+    {
+      film: 'Die Hard',
+      author: 'Hans Gruber',
+      quote:
+        'And when Alexander saw the breadth of his domain, he wept, for there were no more worlds to conquer." Benefits of a classical education.',
+    },
+    {
+      film: 'Die Hard',
+      author: 'Hans Gruber',
+      quote:
+        "I am an exceptional thief, Mrs. McClane. And since I'm moving up to kidnapping, you should be more polite.",
+    },
+    {
+      film: 'Die Hard',
+      author: 'John McClane',
+      quote: "Who's driving this car, Stevie Wonder?",
+    },
+    {
+      film: 'Die Hard',
+      author: 'John McClane',
+      quote: "Drop it, jerk. It's the police",
+    },
+    {
+      film: 'Die Hard',
+      author: 'John McClane',
+      quote: 'Now I know what a TV dinner feels like.',
+    },
+    {
+      film: 'Die Hard',
+      author: 'Theo',
+      quote:
+        'All right, listen up guys. Twas the night before Christmas, and all through the house, not a creature was stirring, except... the four assholes coming in the rear in standard two-by-two cover formation.',
+    },
+    {
+      film: 'Die Hard',
+      author: 'John McClane',
+      quote:
+        "You jerk, I'm gonna kill you! I'm gonna cook you, and I'm gonna eat you!",
+    },
+    {
+      film: 'Die Hard',
+      author: 'John McClane',
+      quote: 'Geronimo, jerks!',
+    },
+    {
+      film: 'Die Hard',
+      author: 'John McClane',
+      quote: 'Happy trails, Hans',
+    },
+    {
+      film: 'Die Hard',
+      author: 'Hans Gruber',
+      quote:
+        "I'm going to count to three, there will not be a four. Give me the code.",
+    },
+    {
+      film: 'Con Air',
+      author: 'Cameron Poe',
+      quote: 'Put... the bunny... back... in the box.',
+    },
+    {
+      film: 'Die Hard 2',
+      author: 'John McClane',
+      quote: 'Take the Twinkie out of your mouth and grab a pencil, will ya?',
+    },
+    {
+      film: 'Die Hard',
+      author: 'John McClane',
+      quote:
+        'Nine million terrorists in the world and I gotta kill one with feet smaller than my sister.',
+    },
+    {
+      film: 'Die Hard',
+      author: 'Hans Gruber',
+      quote:
+        "When they touch down, we'll blow the roof, they'll spend a month sifting through rubble, and by the time they figure out what went wrong, we'll be sitting on a beach, earning twenty percent.",
+    },
+    {
+      film: 'Die Hard',
+      author: 'Hans Gruber',
+      quote:
+        'The circuits that cannot be cut are cut automatically in response to a terrorist incident. You asked for miracles, Theo, I give you the F.B.I.',
+    },
+    {
+      film: 'Die Hard',
+      author: 'Hans Gruber',
+      quote: '*Shoot* the *glass!*',
+    },
+    {
+      film: 'Die Hard',
+      author: 'Hans Gruber',
+      quote: 'Yippie-ki-yay, Mother-----',
+    },
+    {
+      film: 'Die Hard',
+      author: 'Harry Potter',
+      quote: 'Happy Christmas, Harry!',
+    },
   ];
 }
 
--- tests/quotes.test.ts
+++ tests/quotes.test.ts
@@ -1,11 +1,11 @@
 import app from '../src/app';
 import request from 'supertest';
-// import { truncate } from '../truncate';
-// import { prisma } from '../src/utils/db.server';
+import { truncate } from '../truncate';
+import { prisma } from '../src/utils/db.server';
 
-// beforeEach(async () => {
-//   await truncate(['Quotes'], prisma);
-// });
+beforeEach(async () => {
+  await truncate(['Quotes'], prisma);
+});
 it('#POST connects to server', async () => {
   const res = await request(app).post('/quotes');
   expect(res.status).toBe(200);
